c8c537dbd3f28b6b0d4037dbce1bad2c502eaafc
lonewolfyx <877085893@qq.com>
2025-09-05 13:38:25 +0800

    feat: add `Feature3` block

--- app/data/blocks.ts
+++ app/data/blocks.ts
@@ -1,4 +1,46 @@
 export const blocks = {
+    footer2: {
+        files: [
+            {
+                name: 'Footer2',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/footer/footer2/Footer2.vue',
+                raw: () => import('@/registry/blocks/footer/footer2/Footer2.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/footer/footer2/Footer2.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/footer/footer2/Footer2.vue?raw').then(m => m.default),
+    },
+
+    footer1: {
+        files: [
+            {
+                name: 'Footer1',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/footer/footer1/Footer1.vue',
+                raw: () => import('@/registry/blocks/footer/footer1/Footer1.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/footer/footer1/Footer1.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/footer/footer1/Footer1.vue?raw').then(m => m.default),
+    },
+
+    feature3: {
+        files: [
+            {
+                name: 'Feature3',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/feature/feature3/Feature3.vue',
+                raw: () => import('@/registry/blocks/feature/feature3/Feature3.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/feature/feature3/Feature3.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/feature/feature3/Feature3.vue?raw').then(m => m.default),
+    },
+
     feature2: {
         files: [
             {
@@ -39,34 +81,6 @@ export const blocks = {
         className: 'min-w-screen,min-h-screen,flex,justify-center,items-center',
         component: () => import('@/registry/blocks/card/card2/Card2.vue').then(m => m.default),
         raw: () => import('@/registry/blocks/card/card2/Card2.vue?raw').then(m => m.default),
-    },
-
-    footer2: {
-        files: [
-            {
-                name: 'Footer2',
-                target: 'pages/index.vue',
-                path: '@/registry/blocks/footer/footer2/Footer2.vue',
-                raw: () => import('@/registry/blocks/footer/footer2/Footer2.vue?raw').then(m => m.default),
-            },
-        ],
-        className: '',
-        component: () => import('@/registry/blocks/footer/footer2/Footer2.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/footer/footer2/Footer2.vue?raw').then(m => m.default),
-    },
-
-    footer1: {
-        files: [
-            {
-                name: 'Footer1',
-                target: 'pages/index.vue',
-                path: '@/registry/blocks/footer/footer1/Footer1.vue',
-                raw: () => import('@/registry/blocks/footer/footer1/Footer1.vue?raw').then(m => m.default),
-            },
-        ],
-        className: '',
-        component: () => import('@/registry/blocks/footer/footer1/Footer1.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/footer/footer1/Footer1.vue?raw').then(m => m.default),
     },
 
     card1: {
---
+++ app/registry/blocks/feature/feature3/Feature3.vue
@@ -0,0 +1,54 @@
+<template>
+    <section class="py-32">
+        <div class="container mx-auto">
+            <div class="grid gap-8 lg:grid-cols-3">
+                <h2 class="row-span-2 text-3xl font-semibold lg:text-5xl">
+                    Our Values and Principles
+                </h2>
+                <div>
+                    <h3 class="mb-2 text-xl font-medium">
+                        Team Spirit
+                    </h3>
+                    <p class="text-muted-foreground">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+                        architecto atque consequuntur perferendis ratione dolorem vitae,
+                        doloribus facere.
+                    </p>
+                </div>
+                <div>
+                    <h3 class="mb-2 text-xl font-medium">
+                        Innovation
+                    </h3>
+                    <p class="text-muted-foreground">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+                        architecto atque consequuntur perferendis ratione dolorem vitae,
+                        doloribus facere.
+                    </p>
+                </div>
+                <div>
+                    <h3 class="mb-2 text-xl font-medium">
+                        Quality
+                    </h3>
+                    <p class="text-muted-foreground">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+                        architecto atque consequuntur perferendis ratione dolorem vitae,
+                        doloribus facere.
+                    </p>
+                </div>
+                <div>
+                    <h3 class="mb-2 text-xl font-medium">
+                        Integrity
+                    </h3>
+                    <p class="text-muted-foreground">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+                        architecto atque consequuntur perferendis ratione dolorem vitae,
+                        doloribus facere.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+</script>
