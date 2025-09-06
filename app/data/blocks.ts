96e60d206b87f0c16984ebae649d479b30a5a60d
lonewolfyx <877085893@qq.com>
2025-09-06 15:19:08 +0800

    feat: add `Team4` block

--- app/data/blocks.ts
+++ app/data/blocks.ts
@@ -1,4 +1,18 @@
 export const blocks = {
+    team4: {
+        files: [
+            {
+                name: 'Team4',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/team/team4/Team4.vue',
+                raw: () => import('@/registry/blocks/team/team4/Team4.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/team/team4/Team4.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/team/team4/Team4.vue?raw').then(m => m.default),
+    },
+
     team3: {
         files: [
             {
@@ -41,118 +55,6 @@ export const blocks = {
         raw: () => import('@/registry/blocks/team/team1/Team1.vue?raw').then(m => m.default),
     },
 
-    cta1: {
-        files: [
-            {
-                name: 'Cta1',
-                target: 'pages/index.vue',
-                path: '@/registry/blocks/cta/cta1/Cta1.vue',
-                raw: () => import('@/registry/blocks/cta/cta1/Cta1.vue?raw').then(m => m.default),
-            },
-        ],
-        className: '',
-        component: () => import('@/registry/blocks/cta/cta1/Cta1.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/cta/cta1/Cta1.vue?raw').then(m => m.default),
-    },
-
-    cta2: {
-        files: [
-            {
-                name: 'Cta2',
-                target: 'pages/index.vue',
-                path: '@/registry/blocks/cta/cta2/Cta2.vue',
-                raw: () => import('@/registry/blocks/cta/cta2/Cta2.vue?raw').then(m => m.default),
-            },
-        ],
-        className: '',
-        component: () => import('@/registry/blocks/cta/cta2/Cta2.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/cta/cta2/Cta2.vue?raw').then(m => m.default),
-    },
-
-    cta4: {
-        files: [
-            {
-                name: 'Cta4',
-                target: 'pages/index.vue',
-                path: '@/registry/blocks/cta/cta4/Cta4.vue',
-                raw: () => import('@/registry/blocks/cta/cta4/Cta4.vue?raw').then(m => m.default),
-            },
-        ],
-        className: '',
-        component: () => import('@/registry/blocks/cta/cta4/Cta4.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/cta/cta4/Cta4.vue?raw').then(m => m.default),
-    },
-
-    feature3: {
-        files: [
-            {
-                name: 'Feature3',
-                target: 'pages/index.vue',
-                path: '@/registry/blocks/feature/feature3/Feature3.vue',
-                raw: () => import('@/registry/blocks/feature/feature3/Feature3.vue?raw').then(m => m.default),
-            },
-        ],
-        className: '',
-        component: () => import('@/registry/blocks/feature/feature3/Feature3.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/feature/feature3/Feature3.vue?raw').then(m => m.default),
-    },
-
-    cta3: {
-        files: [
-            {
-                name: 'Cta3',
-                target: 'pages/index.vue',
-                path: '@/registry/blocks/cta/cta3/Cta3.vue',
-                raw: () => import('@/registry/blocks/cta/cta3/Cta3.vue?raw').then(m => m.default),
-            },
-        ],
-        className: '',
-        component: () => import('@/registry/blocks/cta/cta3/Cta3.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/cta/cta3/Cta3.vue?raw').then(m => m.default),
-    },
-
-    feature1: {
-        files: [
-            {
-                name: 'Feature1',
-                target: 'pages/index.vue',
-                path: '@/registry/blocks/feature/feature1/Feature1.vue',
-                raw: () => import('@/registry/blocks/feature/feature1/Feature1.vue?raw').then(m => m.default),
-            },
-        ],
-        className: '',
-        component: () => import('@/registry/blocks/feature/feature1/Feature1.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/feature/feature1/Feature1.vue?raw').then(m => m.default),
-    },
-
-    feature4: {
-        files: [
-            {
-                name: 'Feature4',
-                target: 'pages/index.vue',
-                path: '@/registry/blocks/feature/feature4/Feature4.vue',
-                raw: () => import('@/registry/blocks/feature/feature4/Feature4.vue?raw').then(m => m.default),
-            },
-        ],
-        className: '',
-        component: () => import('@/registry/blocks/feature/feature4/Feature4.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/feature/feature4/Feature4.vue?raw').then(m => m.default),
-    },
-
-    feature2: {
-        files: [
-            {
-                name: 'Feature2',
-                target: 'pages/index.vue',
-                path: '@/registry/blocks/feature/feature2/Feature2.vue',
-                raw: () => import('@/registry/blocks/feature/feature2/Feature2.vue?raw').then(m => m.default),
-            },
-        ],
-        className: '',
-        component: () => import('@/registry/blocks/feature/feature2/Feature2.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/feature/feature2/Feature2.vue?raw').then(m => m.default),
-    },
-
     footer4: {
         files: [
             {
@@ -165,20 +67,6 @@ export const blocks = {
         className: '',
         component: () => import('@/registry/blocks/footer/footer4/Footer4.vue').then(m => m.default),
         raw: () => import('@/registry/blocks/footer/footer4/Footer4.vue?raw').then(m => m.default),
-    },
-
-    footer2: {
-        files: [
-            {
-                name: 'Footer2',
-                target: 'pages/index.vue',
-                path: '@/registry/blocks/footer/footer2/Footer2.vue',
-                raw: () => import('@/registry/blocks/footer/footer2/Footer2.vue?raw').then(m => m.default),
-            },
-        ],
-        className: '',
-        component: () => import('@/registry/blocks/footer/footer2/Footer2.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/footer/footer2/Footer2.vue?raw').then(m => m.default),
     },
 
     footer3: {
@@ -195,6 +83,20 @@ export const blocks = {
         raw: () => import('@/registry/blocks/footer/footer3/Footer3.vue?raw').then(m => m.default),
     },
 
+    footer2: {
+        files: [
+            {
+                name: 'Footer2',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/footer/footer2/Footer2.vue',
+                raw: () => import('@/registry/blocks/footer/footer2/Footer2.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/footer/footer2/Footer2.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/footer/footer2/Footer2.vue?raw').then(m => m.default),
+    },
+
     footer1: {
         files: [
             {
@@ -207,6 +109,118 @@ export const blocks = {
         className: '',
         component: () => import('@/registry/blocks/footer/footer1/Footer1.vue').then(m => m.default),
         raw: () => import('@/registry/blocks/footer/footer1/Footer1.vue?raw').then(m => m.default),
+    },
+
+    feature4: {
+        files: [
+            {
+                name: 'Feature4',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/feature/feature4/Feature4.vue',
+                raw: () => import('@/registry/blocks/feature/feature4/Feature4.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/feature/feature4/Feature4.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/feature/feature4/Feature4.vue?raw').then(m => m.default),
+    },
+
+    feature3: {
+        files: [
+            {
+                name: 'Feature3',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/feature/feature3/Feature3.vue',
+                raw: () => import('@/registry/blocks/feature/feature3/Feature3.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/feature/feature3/Feature3.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/feature/feature3/Feature3.vue?raw').then(m => m.default),
+    },
+
+    feature2: {
+        files: [
+            {
+                name: 'Feature2',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/feature/feature2/Feature2.vue',
+                raw: () => import('@/registry/blocks/feature/feature2/Feature2.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/feature/feature2/Feature2.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/feature/feature2/Feature2.vue?raw').then(m => m.default),
+    },
+
+    feature1: {
+        files: [
+            {
+                name: 'Feature1',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/feature/feature1/Feature1.vue',
+                raw: () => import('@/registry/blocks/feature/feature1/Feature1.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/feature/feature1/Feature1.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/feature/feature1/Feature1.vue?raw').then(m => m.default),
+    },
+
+    cta4: {
+        files: [
+            {
+                name: 'Cta4',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/cta/cta4/Cta4.vue',
+                raw: () => import('@/registry/blocks/cta/cta4/Cta4.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/cta/cta4/Cta4.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/cta/cta4/Cta4.vue?raw').then(m => m.default),
+    },
+
+    cta3: {
+        files: [
+            {
+                name: 'Cta3',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/cta/cta3/Cta3.vue',
+                raw: () => import('@/registry/blocks/cta/cta3/Cta3.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/cta/cta3/Cta3.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/cta/cta3/Cta3.vue?raw').then(m => m.default),
+    },
+
+    cta1: {
+        files: [
+            {
+                name: 'Cta1',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/cta/cta1/Cta1.vue',
+                raw: () => import('@/registry/blocks/cta/cta1/Cta1.vue?raw').then(m => m.default),
+            },
+        ],
+        className: '',
+        component: () => import('@/registry/blocks/cta/cta1/Cta1.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/cta/cta1/Cta1.vue?raw').then(m => m.default),
+    },
+
+    card2: {
+        files: [
+            {
+                name: 'Card2',
+                target: 'pages/index.vue',
+                path: '@/registry/blocks/card/card2/Card2.vue',
+                raw: () => import('@/registry/blocks/card/card2/Card2.vue?raw').then(m => m.default),
+            },
+        ],
+        className: 'min-w-screen,min-h-screen,flex,justify-center,items-center',
+        component: () => import('@/registry/blocks/card/card2/Card2.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/card/card2/Card2.vue?raw').then(m => m.default),
     },
 
     card1: {
@@ -223,17 +237,17 @@ export const blocks = {
         raw: () => import('@/registry/blocks/card/card1/Card1.vue?raw').then(m => m.default),
     },
 
-    card2: {
+    cta2: {
         files: [
             {
-                name: 'Card2',
+                name: 'Cta2',
                 target: 'pages/index.vue',
-                path: '@/registry/blocks/card/card2/Card2.vue',
-                raw: () => import('@/registry/blocks/card/card2/Card2.vue?raw').then(m => m.default),
+                path: '@/registry/blocks/cta/cta2/Cta2.vue',
+                raw: () => import('@/registry/blocks/cta/cta2/Cta2.vue?raw').then(m => m.default),
             },
         ],
-        className: 'min-w-screen,min-h-screen,flex,justify-center,items-center',
-        component: () => import('@/registry/blocks/card/card2/Card2.vue').then(m => m.default),
-        raw: () => import('@/registry/blocks/card/card2/Card2.vue?raw').then(m => m.default),
+        className: '',
+        component: () => import('@/registry/blocks/cta/cta2/Cta2.vue').then(m => m.default),
+        raw: () => import('@/registry/blocks/cta/cta2/Cta2.vue?raw').then(m => m.default),
     },
 }
---
+++ app/registry/blocks/team/team4/Team4.vue
@@ -0,0 +1,120 @@
+<template>
+    <section class="py-32">
+        <div class="container">
+            <div class="flex flex-col gap-6 py-4 lg:py-8">
+                <Badge
+                    class="w-fit gap-1 bg-card px-3 text-sm font-normal tracking-tight shadow-sm"
+                    variant="outline"
+                >
+                    <Icon
+                        class="size-4"
+                        icon="lucide:users"
+                    />
+                    <span>Team up!</span>
+                </Badge>
+                <h2 class="text-3xl leading-tight tracking-tight md:text-4xl lg:text-6xl">
+                    The Minds Behind the Mission
+                </h2>
+                <p class="max-w-[600px] tracking-[-0.32px] text-muted-foreground">
+                    A dedicated team passionate about shaping the future of
+                    productivity.
+                </p>
+            </div>
+
+            <div class="mt-10 grid gap-x-12 gap-y-16 sm:grid-cols-2 md:mt-14 lg:grid-cols-4">
+                <div
+                    v-for="member in teamMembers"
+                    :key="member.name"
+                    class="group flex flex-col"
+                >
+                    <img
+                        :height="80"
+                        :src="member.avatar"
+                        :width="80"
+                        alt="member.name"
+                        class="rounded-full object-contain"
+                    >
+                    <div class="mt-6 flex flex-col tracking-[-0.32px]">
+                        <h3 class="text-lg">
+                            {{ member.name }}
+                        </h3>
+                        <p class="text-muted-foreground-subtle">
+                            {{ member.role }}
+                        </p>
+                        <p class="mt-4 text-sm tracking-[-0.36px] text-muted-foreground">
+                            {{ member.bio }}
+                        </p>
+                        <div class="mt-6 flex gap-2">
+                            <a
+                                v-if="member.social.twitter"
+                                :href="member.social.twitter"
+                                class="hover:text-foreground"
+                            >
+                                <Icon
+                                    class="size-4"
+                                    icon="lucide:twitter"
+                                />
+                            </a>
+                            <a
+                                v-if="member.social.github"
+                                :href="member.social.github"
+                                class="hover:text-foreground"
+                            >
+                                <Icon
+                                    class="size-4"
+                                    icon="lucide:github"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue'
+import { Badge } from '@/components/ui/badge'
+
+const teamMembers = [
+    {
+        name: 'Alex Johnson',
+        role: 'CEO & Founder',
+        avatar: 'https://mockmind-api.uifaces.co/content/human/80.jpg',
+        bio: 'With a background in software development and a vision for productivity, Alex leads the team with passion on user-first innovation.',
+        social: {
+            twitter: '#',
+            github: '#',
+        },
+    },
+    {
+        name: 'Jamie Lee',
+        role: 'Chief Product Officer',
+        avatar: 'https://mockmind-api.uifaces.co/content/human/125.jpg',
+        bio: 'Jamie brings years of experience in product design and strategy, ensuring each feature meets the highest standards of functionality and design. ',
+        social: {
+            twitter: '#',
+        },
+    },
+    {
+        name: 'Taylor Smith',
+        role: 'Head of Engineering',
+        avatar: 'https://mockmind-api.uifaces.co/content/human/104.jpg',
+        bio: 'Taylor drives the technical vision, overseeing development and ensuring the product is robust, secure, and scalable for users worldwide.',
+        social: {
+            twitter: '#',
+            github: '#',
+        },
+    },
+    {
+        name: 'Morgan Davis',
+        role: 'Marketing Lead',
+        avatar: 'https://mockmind-api.uifaces.co/content/human/108.jpg',
+        bio: 'With a knack for storytelling and a deep understanding of the market, Morgan communicates our mission and product benefits to the world.',
+        social: {
+            twitter: '#',
+        },
+    },
+]
+</script>
