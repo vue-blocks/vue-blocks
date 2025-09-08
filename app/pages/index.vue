4083afcc9930f8a3a4cd88a1352f00b743a1b7f1
lonewolfyx <877085893@qq.com>
2025-09-08 16:26:10 +0800

    refactor(ui): homepage layout with new design

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,7 @@
 <template>
-    <!--    <AppHero /> -->
-    <AppBlocks />
-    <AppFeature />
-<!--    <AppContributions /> -->
+    <AppHero />
 </template>
 
 <script lang="ts" setup>
-import AppHero from '~/components/AppHero.vue'
-import AppBlocks from '~/components/AppBlocks.vue'
-import AppFeature from '~/components/AppFeature.vue'
-import AppContributions from '~/components/AppContributions.vue'
+import AppHero from '@/components/AppHero.vue'
 </script>
