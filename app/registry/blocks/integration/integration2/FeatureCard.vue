45f9211cb750c84919e756628afc0bf59aae07b0
lonewolfyx <877085893@qq.com>
2025-09-19 11:23:15 +0800

    feat: add `integration2` block

---
+++ app/registry/blocks/integration/integration2/FeatureCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <Card class="bg-background w-full rounded-xl py-2">
+        <CardHeader class="flex flex-row items-center space-x-4">
+            <div class="relative h-12 w-12 shrink-0">
+                <img
+                    :alt="title"
+                    :src="icon"
+                    :width="48"
+                    class="h-full w-full object-contain"
+                    height="48"
+                >
+            </div>
+            <div>
+                <CardTitle class="text-lg">
+                    {{ title }}
+                </CardTitle>
+                <CardDescription class="text-muted-foreground">
+                    {{ description }}
+                </CardDescription>
+            </div>
+        </CardHeader>
+    </Card>
+</template>
+
+<script lang="ts" setup>
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+defineProps<{
+    title: string
+    description: string
+    icon: string
+}>()
+</script>
