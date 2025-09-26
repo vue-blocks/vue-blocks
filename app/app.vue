738ca2731297342ca6abbf7f3608331077bbea92
OldDriver <olddrivero.king@qq.com>
2025-09-26 19:11:38 +0800

    feat: add `login1` block (#36)

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,11 @@
 <template>
     <NuxtLayout>
+        <Toaster />
         <NuxtPage />
     </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { Toaster } from '@/components/ui/sonner'
+import 'vue-sonner/style.css'
+</script>
