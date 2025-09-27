2d7944b9c7a83f338c08ade77504e23e7e27d782
lonewolfyx <877085893@qq.com>
2025-09-27 20:33:22 +0800

    feat: add `CaseStudy1` block

---
+++ app/registry/blocks/casestudy/CaseStudy1/CaseStudy1.vue
@@ -0,0 +1,212 @@
+<template>
+    <section class="py-32">
+        <div class="container">
+            <div class="mx-auto max-w-7xl">
+                <Breadcrumb class="mb-6 lg:mb-10">
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/">
+                                Home
+                            </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator>/</BreadcrumbSeparator>
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>Components</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
+                <div class="relative flex-col gap-10 lg:flex lg:flex-row lg:justify-between">
+                    <div class="lg:max-w-[692px]">
+                        <div class="max lg:col-span-2">
+                            <div>
+                                <h1 class="text-pretty text-3xl font-extrabold">
+                                    Boosting System Reliability by 125% with AI Monitoring
+                                </h1>
+                                <p class="text-muted-foreground mt-2 text-lg">
+                                    In a kingdom far away, where laughter once flowed freely, a
+                                    peculiar tale unfolded about a king who decided to tax the
+                                    very essence of joy itself - jokes and jest.
+                                </p>
+                                <img
+                                    alt="placeholder"
+                                    class="my-8 aspect-video w-full rounded-lg object-cover"
+                                    src="https://github.com/vue-blocks/static/blob/main/placeholder-1.png?raw=true"
+                                >
+                                <div class="mb-8 grid grid-cols-2 gap-5 lg:grid-cols-4">
+                                    <div class="flex flex-col gap-2">
+                                        <p class="text-4xl font-semibold sm:text-5xl">
+                                            19%
+                                        </p>
+                                        <p class="text-muted-foreground text-sm">
+                                            increase in user engagement rate
+                                        </p>
+                                    </div>
+                                    <div class="flex flex-col gap-2">
+                                        <p class="text-4xl font-semibold sm:text-5xl">
+                                            28%
+                                        </p>
+                                        <p class="text-muted-foreground text-sm">
+                                            growth in customer retention rate
+                                        </p>
+                                    </div>
+                                    <div class="flex flex-col gap-2">
+                                        <p class="text-4xl font-semibold sm:text-5xl">
+                                            72%
+                                        </p>
+                                        <p class="text-muted-foreground text-sm">
+                                            satisfaction rate among users and customers
+                                        </p>
+                                    </div>
+                                    <div class="flex flex-col gap-2">
+                                        <p class="text-4xl font-semibold sm:text-5xl">
+                                            &gt;85%
+                                        </p>
+                                        <p class="text-muted-foreground text-sm">
+                                            positive feedback received from users
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="prose dark:prose-invert mb-8 max-w-full lg:max-w-prose">
+                                <h2>How the Tax System Works</h2>
+                                <p>
+                                    The king, seeing how much happier his subjects were,
+                                    realized the error of his ways and repealed the joke tax.
+                                    Jokester was declared a hero, and the kingdom lived happily
+                                    ever after.
+                                </p>
+                                <Alert>
+                                    <Lightbulb class="h-4 w-4" />
+                                    <AlertTitle>Royal Decree!</AlertTitle>
+                                    <AlertDescription>
+                                        Remember, all jokes must be registered at the Royal Jest
+                                        Office before telling them
+                                    </AlertDescription>
+                                </Alert>
+
+                                <h2>The People&apos;s Rebellion</h2>
+                                <p>
+                                    The people of the kingdom, feeling uplifted by the laughter,
+                                    started to tell jokes and puns again, and soon the entire
+                                    kingdom was in on the joke.
+                                </p>
+                                <div>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>King&apos;s Treasury</th>
+                                                <th>People&apos;s happiness</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Empty</td>
+                                                <td>Overflowing</td>
+                                            </tr>
+                                            <tr class="even:bg-muted m-0 border-t p-0">
+                                                <td>Modest</td>
+                                                <td>Satisfied</td>
+                                            </tr>
+                                            <tr class="even:bg-muted m-0 border-t p-0">
+                                                <td>Full</td>
+                                                <td>Ecstatic</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <p>
+                                    The king, seeing how much happier his subjects were,
+                                    realized the error of his ways and repealed the joke tax.
+                                    Jokester was declared a hero, and the kingdom lived happily
+                                    ever after.
+                                </p>
+
+                                <h2>The King&apos;s Plan</h2>
+                                <p>
+                                    The king thought long and hard, and finally came up with{" "}
+                                    <a href="#">a brilliant plan</a>: he would tax the jokes in
+                                    the kingdom.
+                                </p>
+                                <blockquote>
+                                    &ldquo;After all,&rdquo; he said, &ldquo;everyone enjoys a
+                                    good joke, so it&apos;s only fair that they should pay for
+                                    the privilege.&rdquo;
+                                </blockquote>
+                                <p>
+                                    The king&apos;s subjects were not amused. They grumbled and
+                                    complained, but the king was firm:
+                                </p>
+                                <ul>
+                                    <li>1st level of puns: 5 gold coins</li>
+                                    <li>2nd level of jokes: 10 gold coins</li>
+                                    <li>3rd level of one-liners : 20 gold coins</li>
+                                </ul>
+                                <p>
+                                    As a result, people stopped telling jokes, and the kingdom
+                                    fell into a gloom. But there was one person who refused to
+                                    let the king&apos;s foolishness get him down: a court jester
+                                    named Jokester.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="h-fit lg:sticky lg:top-8 lg:max-w-80">
+                        <img
+                            alt="logo"
+                            class="mb-8 w-36"
+                            src="https://raw.githubusercontent.com/vue-blocks/vue-blocks/refs/heads/master/public/vue-blocks.svg"
+                        >
+                        <p class="mb-1.5 text-sm font-semibold">
+                            Overview
+                        </p>
+                        <p class="text-muted-foreground mb-5 text-sm">
+                            Our client implemented our solution to transform their business
+                            operations, resulting in improved efficiency, enhanced customer
+                            experience, and significant cost savings across their entire
+                            organization.
+                        </p>
+                        <p class="mb-1.5 text-sm font-semibold">
+                            Sector
+                        </p>
+                        <p class="text-muted-foreground mb-5 text-sm">
+                            Technology
+                        </p>
+                        <p class="mb-1.5 text-sm font-semibold">
+                            Solution
+                        </p>
+                        <Button
+                            size="sm"
+                            variant="outline"
+                        >
+                            <GitBranch class="opacity-60" />
+                            Enterprise
+                        </Button>
+                        <Separator class="my-5" />
+                        <p class="mb-3 text-sm font-semibold">
+                            Want to learn more?
+                        </p>
+                        <Button size="sm">
+                            Contact us
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+import { GitBranch, Lightbulb } from 'lucide-vue-next'
+
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+</script>
