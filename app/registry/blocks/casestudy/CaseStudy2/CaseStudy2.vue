13c7b87977d46335f4b34173e2de9055e46b97a2
lonewolfyx <877085893@qq.com>
2025-09-27 20:45:40 +0800

    feat: add `CaseStudy2` block

---
+++ app/registry/blocks/casestudy/CaseStudy2/CaseStudy2.vue
@@ -0,0 +1,169 @@
+<template>
+    <section class="py-32">
+        <div class="container flex flex-col gap-12 lg:flex-row lg:gap-24">
+            <article class="mx-auto">
+                <img
+                    src="https://github.com/vue-blocks/static/blob/main/placeholder-1.png?raw=true"
+                    alt="placeholder"
+                    class="mb-8 aspect-video w-full max-w-3xl rounded-lg border object-cover"
+                >
+                <div class="prose dark:prose-invert">
+                    <h1>How Mercury uses shadcn to build their design system</h1>
+                    <p>
+                        Once upon a time, in a far-off land, there was a very lazy king
+                        who spent all day lounging on his throne. One day, his advisors
+                        came to him with a problem: the kingdom was running out of money.
+                    </p>
+                    <h2>The King&apos;s Plan</h2>
+                    <p>
+                        The king thought long and hard, and finally came up with {{ " " }}
+                        <a href="#">a brilliant plan</a>: he would tax the jokes in the
+                        kingdom.
+                    </p>
+                    <blockquote>
+                        &ldquo;After all,&rdquo; he said, &ldquo;everyone enjoys a good
+                        joke, so it&apos;s only fair that they should pay for the
+                        privilege.&rdquo;
+                    </blockquote>
+                    <h3>The Joke Tax</h3>
+                    <p>
+                        The king&apos;s subjects were not amused. They grumbled and
+                        complained, but the king was firm:
+                    </p>
+                    <ul>
+                        <li>1st level of puns: 5 gold coins</li>
+                        <li>2nd level of jokes: 10 gold coins</li>
+                        <li>3rd level of one-liners : 20 gold coins</li>
+                    </ul>
+                    <p>
+                        As a result, people stopped telling jokes, and the kingdom fell
+                        into a gloom. But there was one person who refused to let the
+                        king&apos;s foolishness get him down: a court jester named
+                        Jokester.
+                    </p>
+                    <h3>Jokester&apos;s Revolt</h3>
+                    <p>
+                        Jokester began sneaking into the castle in the middle of the night
+                        and leaving jokes all over the place: under the king&apos;s
+                        pillow, in his soup, even in the royal toilet. The king was
+                        furious, but he couldn&apos;t seem to stop Jokester.
+                    </p>
+                    <p>
+                        And then, one day, the people of the kingdom discovered that the
+                        jokes left by Jokester were so funny that they couldn&apos;t help
+                        but laugh. And once they started laughing, they couldn&apos;t
+                        stop.
+                    </p>
+                    <h3>The People&apos;s Rebellion</h3>
+                    <p>
+                        The people of the kingdom, feeling uplifted by the laughter,
+                        started to tell jokes and puns again, and soon the entire kingdom
+                        was in on the joke.
+                    </p>
+                    <div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>King&apos;s Treasury</th>
+                                    <th>People&apos;s happiness</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Empty</td>
+                                    <td>Overflowing</td>
+                                </tr>
+                                <tr class="even:bg-muted m-0 border-t p-0">
+                                    <td>Modest</td>
+                                    <td>Satisfied</td>
+                                </tr>
+                                <tr class="even:bg-muted m-0 border-t p-0">
+                                    <td>Full</td>
+                                    <td>Ecstatic</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <p>
+                        The king, seeing how much happier his subjects were, realized the
+                        error of his ways and repealed the joke tax. Jokester was declared
+                        a hero, and the kingdom lived happily ever after.
+                    </p>
+                    <p>
+                        The moral of the story is: never underestimate the power of a good
+                        laugh and always be careful of bad ideas.
+                    </p>
+                </div>
+            </article>
+
+            <aside class="lg:max-w-[300px]">
+                <div class="border-border bg-accent flex flex-col items-start rounded-lg border py-6 md:py-8">
+                    <div class="mb-8 px-6">
+                        <img
+                            src="https://github.com/vue-blocks/vue-blocks/raw/refs/heads/master/public/vue-blocks.svg?raw=true"
+                            alt="placeholder"
+                            class="max-h-8 w-full"
+                        >
+                    </div>
+                    <div class="mb-5 px-6 last:mb-0">
+                        <div class="mb-2 text-xs font-semibold">
+                            Company
+                        </div>
+                        <div class="text-muted-foreground overflow-hidden text-xs md:text-sm">
+                            Suspendisse vel euismod sem. Sed sollicitudin augue eu facilisis
+                            scelerisque. Nullam pharetra tortor ut massa accumsan egestas.
+                        </div>
+                    </div>
+                    <div class="mb-5 px-6 last:mb-0">
+                        <div class="mb-2 text-xs font-semibold">
+                            Industry
+                        </div>
+                        <div class="text-muted-foreground overflow-hidden text-xs md:text-sm">
+                            Suspendisse volutpat
+                        </div>
+                    </div>
+                    <div class="border-border mb-5 w-full border-t px-6 pt-5 last:mb-0">
+                        <div class="mb-2 text-xs font-semibold">
+                            Location
+                        </div>
+                        <div class="text-muted-foreground overflow-hidden text-xs md:text-sm">
+                            London, United Kingdom
+                        </div>
+                    </div>
+                    <div class="mb-5 px-6 last:mb-0">
+                        <div class="mb-2 text-xs font-semibold">
+                            Company size
+                        </div>
+                        <div class="text-muted-foreground overflow-hidden text-xs md:text-sm">
+                            11-50
+                        </div>
+                    </div>
+                    <div class="mb-5 px-6 last:mb-0">
+                        <div class="mb-2 text-xs font-semibold">
+                            Website
+                        </div>
+                        <div class="text-muted-foreground overflow-hidden text-xs md:text-sm">
+                            <a
+                                href="#"
+                                class="hover:text-foreground underline"
+                            >
+                                https://vue-blocks.dev/
+                            </a>
+                        </div>
+                    </div>
+                    <div class="mb-5 px-6 last:mb-0">
+                        <div class="mb-2 text-xs font-semibold">
+                            Topics
+                        </div>
+                        <div class="text-muted-foreground overflow-hidden text-xs md:text-sm">
+                            Sed sollicitudin augue eu facilisis scelerisque
+                        </div>
+                    </div>
+                </div>
+            </aside>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+</script>
